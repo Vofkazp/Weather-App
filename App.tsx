7acb2d9c822a0e33e9e692b5e296fbbd6528d9f8
Vofka <vofkazp@gmail.com>
2025-06-13 14:30:19 +0300

    fix 2 finish commit

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import TemperatureContextProvider from "./context/TemperatureContext";
 import RootNavigator from "./components/RootNavigator";
 import {BACKGROUND_COLOR} from "./constants/colors";
 import {StatusBar} from "expo-status-bar";
+import {enableScreens} from 'react-native-screens';
+enableScreens();
 
 export default function App() {
   return (
